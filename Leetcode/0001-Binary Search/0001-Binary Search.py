e198ad14679f838bffde4492edcf5df74e82da6d
SosiSis <sosinasisay30@gmail.com>
2025-01-24 10:16:03 +0300

    solving binary search

---
+++ Leetcode/0001-Binary Search/0001-Binary Search.py
@@ -0,0 +1,19 @@
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 0:
+            return -1
+        
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                low = mid + 1
+            else:  # target < nums[mid]
+                high = mid - 1
+        
+        return -1
