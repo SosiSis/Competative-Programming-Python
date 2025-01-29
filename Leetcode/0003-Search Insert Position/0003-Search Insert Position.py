5c0ce7932032105b016d688eda5778be253061a9
SosiSis <sosinasisay30@gmail.com>
2025-01-29 07:48:21 +0300

    Search Insert Position

---
+++ Leetcode/0003-Search Insert Position/0003-Search Insert Position.py
@@ -0,0 +1,17 @@
+from typing import List  # Import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if len(nums)<0:
+            return -1
+        low = 0
+        high= len(nums) - 1
+        while low <= high:
+            mid= (low + high) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                high=mid-1
+            else:
+                low=mid+1
+        return low      
