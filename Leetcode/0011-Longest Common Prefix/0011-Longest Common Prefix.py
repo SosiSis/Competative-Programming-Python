da74ad4c17a8123c4da1416c7f3eac74fe520d82
SosiSis <sosinasisay30@gmail.com>
2025-02-05 10:12:10 +0300

    Longest Common Prefix

--- Leetcode/0011-Longest Common Prefix/0011-Longest Common Prefix.py
+++ Leetcode/0011-Longest Common Prefix/0011-Longest Common Prefix.py
@@ -5,12 +5,14 @@ class Solution(object):
         :rtype: str
         """
 
-        if not strs:  # Handle empty input
+        if not strs:
             return ""
 
-        # Use zip to compare characters at the same index across all strings
-        for i, chars in enumerate(zip(*strs)): #zip(*strs) transposes the list of strings
-            if len(set(chars)) != 1: #check if all the charachters in the tuple are equal
-                return strs[0][:i] #if not equal return the prefix from the first string till the unmatched index
+        shortest_str = min(strs, key=len)  # Find the shortest string
+        for i in range(len(shortest_str)):
+            char = shortest_str[i]
+            for other_str in strs:
+                if i >= len(other_str) or other_str[i] != char:
+                    return shortest_str[:i]  # Return prefix up to mismatch
 
-        return strs[0]  # If all characters match, the shortest string is the prefix
+        return shortest_str  # If all characters match, shortest is the prefix
