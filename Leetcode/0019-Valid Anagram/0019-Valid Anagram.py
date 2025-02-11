4ec56225cebb39552ab00e6caf75627e02ec2f6b
SosiSis <sosinasisay30@gmail.com>
2025-02-11 07:27:23 +0300

    Valid Anagram

---
+++ Leetcode/0019-Valid Anagram/0019-Valid Anagram.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+         return False
+        hashmap = Counter(s) # TC for Counter is O(n)
+
+        for ch in t:
+            if hashmap[ch] > 0:
+                hashmap[ch]-=1
+            else:
+                return False
+        return True
+        
