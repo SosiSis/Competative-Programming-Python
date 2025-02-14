0c23ce79fcd3bf35b7863a35ea143b6951f7e94d
SosiSis <sosinasisay30@gmail.com>
2025-02-14 08:13:30 +0300

    Find the Winner of the Circular Game

--- Leetcode/0022-Find the Winner of the Circular Game/0022-Find the Winner of the Circular Game.py
+++ Leetcode/0022-Find the Winner of the Circular Game/0022-Find the Winner of the Circular Game.py
@@ -5,4 +5,19 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        if n == 1:
+            return 1
+
+        winner = self.findTheWinner(n - 1, k)
+        return (winner + k - 1) % n + 1
+
+# class Solution:
+#     def findTheWinner(self, n: int, k: int) -> int:
+#         friends = list(range(1, n + 1))
+#         index = 0
         
+#         while len(friends) > 1:
+#             index = (index + k - 1) % len(friends)
+#             friends.pop(index)
+        
+#         return friends[0]
