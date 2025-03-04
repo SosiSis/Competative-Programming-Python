6c3a9d4145233e13fa81bb33a9e988f87642d830
SosiSis <sosinasisay30@gmail.com>
2025-03-04 07:58:05 +0300

    Find Greatest Common Divisor of Array

---
+++ Leetcode/0038-Find Greatest Common Divisor of Array/0038-Find Greatest Common Divisor of Array.py
@@ -0,0 +1,26 @@
+
+class Solution(object):
+    def findGCD(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        min_num = min(nums)
+        max_num = max(nums)
+
+        def gcd(a, b):
+            """
+            Calculates the greatest common divisor of two numbers using Euclid's algorithm.
+
+            Args:
+              a: The first number.
+              b: The second number.
+
+            Returns:
+              The greatest common divisor of a and b.
+            """
+            while b:
+                a, b = b, a % b
+            return a
+
+        return gcd(min_num, max_num)
