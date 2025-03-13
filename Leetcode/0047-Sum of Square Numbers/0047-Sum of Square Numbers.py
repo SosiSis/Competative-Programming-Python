d352b2dab7b5bd1fb01600b324809baabc34574d
SosiSis <sosinasisay30@gmail.com>
2025-03-13 08:02:01 +0300

    Sum of Square Numbers

--- Leetcode/0047-Sum of Square Numbers/0047-Sum of Square Numbers.py
+++ Leetcode/0047-Sum of Square Numbers/0047-Sum of Square Numbers.py
@@ -1,3 +1,16 @@
+import math
+
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-     pass
+        a, b = 0, int(math.sqrt(c))
+        
+        while a <= b:
+            sum_of_squares = a * a + b * b
+            if sum_of_squares == c:
+                return True
+            elif sum_of_squares < c:
+                a += 1
+            else:
+                b -= 1
+        
+        return False
