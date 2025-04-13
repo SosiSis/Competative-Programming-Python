2a0212cb3a44a1fd68e7de62c48fd2117fab9567
SosiSis <sosinasisay30@gmail.com>
2025-04-13 09:27:20 +0300

    Find the Duplicate Number

--- Leetcode/0037-Find the Duplicate Number/0037-Find the Duplicate Number.py
+++ Leetcode/0037-Find the Duplicate Number/0037-Find the Duplicate Number.py
@@ -1,2 +1,17 @@
-# 
-print(7%2)
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        slow, fast = nums[0], nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+        slow2 = 0
+        while slow != slow2:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+
+        return slow
