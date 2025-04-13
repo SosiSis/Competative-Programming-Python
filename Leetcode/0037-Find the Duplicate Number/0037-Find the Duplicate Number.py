ec18efe550e91f74e8dc87cbc233368a41b1d32f
SosiSis <sosinasisay30@gmail.com>
2025-04-13 09:48:29 +0300

    Find the Duplicate Number

--- Leetcode/0037-Find the Duplicate Number/0037-Find the Duplicate Number.py
+++ Leetcode/0037-Find the Duplicate Number/0037-Find the Duplicate Number.py
@@ -15,3 +15,21 @@ class Solution(object):
             slow2 = nums[slow2]
 
         return slow
+    
+
+
+
+#     nums = [1, 3, 4, 2, 2]
+#             ↑  ↑  ↑  ↑  ↑
+#     index   0  1  2  3  4
+# The value at each index tells you where to go next. So you can treat this array like a "linked list" where:
+
+# index 0 has value 1 → go to index 1
+
+# index 1 has value 3 → go to index 3
+
+# index 3 has value 2 → go to index 2
+
+# index 2 has value 4 → go to index 4
+
+# index 4 has value 2 → go to index 2 (again!)
